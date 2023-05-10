251232b44d5097293f379e34d4c713743e6d4dd2
Eduardo <eduardo.moura@bestpontos.com.br>
2023-05-10 15:34:23 -0300

    fist commit

---
+++ src/test/java/com/management/task/api/TaskManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.management.task.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
